d20f97ca6ddcc1834839c1acd2e61e7c3bc77ef2
Gaurav-102005 <gauravbhasker10@gmail.com>
2026-08-18 13:45:22 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+// let score = null
+// let score = undefined
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(score)
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// 1 => true; 0 => false;
+// "" => false
+// "gaurav" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
